08d4e3694a8da2c893ba51694142f9e84db019f2
zouzhangwei <wearecisco@gmail.com>
2020-03-10 02:45:44 +0800

    add create project dir 'src'

--- src/main/java/com/wonderingwall/plugin/shellcommand/ShellCommandModuleBuilder.java
+++ src/main/java/com/wonderingwall/plugin/shellcommand/ShellCommandModuleBuilder.java
@@ -5,15 +5,44 @@ import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShellCommandModuleBuilder extends ModuleBuilder {
+
+    private List<Pair<String,String>> mySourcePaths;
 
     @Override
     public void setupRootModel(@NotNull ModifiableRootModel modifiableRootModel) throws ConfigurationException {
         super.setupRootModel(modifiableRootModel);
+        // 创建目录
+        ContentEntry contentEntry = doAddContentEntry(modifiableRootModel);
+        if (contentEntry != null) {
+            final List<Pair<String,String>> sourcePaths = getSourcePaths();
+
+            if (sourcePaths != null) {
+                for (final Pair<String, String> sourcePath : sourcePaths) {
+                    String first = sourcePath.first;
+                    new File(first).mkdirs();
+                    final VirtualFile sourceRoot = LocalFileSystem.getInstance()
+                            .refreshAndFindFileByPath(FileUtil.toSystemIndependentName(first));
+                    if (sourceRoot != null) {
+                        contentEntry.addSourceFolder(sourceRoot, false, sourcePath.second);
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -21,4 +50,20 @@ public class ShellCommandModuleBuilder extends ModuleBuilder {
         return ShellCommandModuleType.getInstance();
     }
 
+
+    private List<Pair<String,String>> getSourcePaths() {
+        if (mySourcePaths == null) {
+            final List<Pair<String, String>> paths = new ArrayList<>();
+            @NonNls final String path = mkdir("src");
+            paths.add(Pair.create(path, ""));
+            return paths;
+        }
+        return mySourcePaths;
+    }
+
+    private String mkdir(String name) {
+        @NonNls final String path = getContentEntryPath() + (name.startsWith(File.separator) ? name : File.separator + name);
+        new File(path).mkdirs();
+        return path;
+    }
 }
